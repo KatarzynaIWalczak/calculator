92aaef3ad87ad4cba2438aba2493b4a99ab46238
KatarzynaIWalczak <k.walczak89@gmail.com>
2024-08-19 03:30:43 +0200

    Added handle button click functionality

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,6 +9,7 @@ export class Calculator {
 
   init() {
     this.addButtons();
+    this.addEventListeners();
   }
 
   addButtons() {
@@ -16,5 +17,46 @@ export class Calculator {
       (button) =>
         `<button class="${button.class}" data-action="${button.action}">${button.value}</button>`
     ).join("");
+  }
+
+  addEventListeners() {
+    this.buttonsContainer.removeEventListener("click", (e) =>
+      this.handleButtonClick(e)
+    );
+    this.buttonsContainer.addEventListener("click", (e) =>
+      this.handleButtonClick(e)
+    );
+  }
+
+  handleButtonClick(e) {
+    e.preventDefault();
+    if (e.target.tagName !== "BUTTON") return;
+
+    const selectedValue = e.target.innerText;
+    const selectedAction = e.target.getAttribute("data-action");
+
+    switch (selectedAction) {
+      case "delete-last":
+        this.deletePreviousValue();
+        break;
+      case "clear-all":
+        this.clearAll();
+        break;
+      case "equals":
+        console.log(selectedValue, selectedAction);
+        break;
+      default:
+        this.screen.value += selectedValue;
+        return;
+    }
+  }
+
+  deletePreviousValue() {
+    this.screen.value !== "0" &&
+      (this.screen.value = this.screen.value.slice(0, -1));
+  }
+
+  clearAll() {
+    this.screen.value = "0";
   }
 }
